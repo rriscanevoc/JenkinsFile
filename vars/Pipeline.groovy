0c07d3a5c24d22e6b96c487167ce836701e853a9
Ricardo Riscanevo Cotrina <rriscanevoc@ccxc.co>
2025-03-18 12:50:37 -0500

    delete parameter SonarQube URL

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -50,16 +50,6 @@ def call() {
                     }
                 }
             }
-            /*stage('Imprimir') {
-                steps {
-                    script {
-                        withCredentials([string(credentialsId: 'Sonar', variable: 'sonartoken')]) {
-                            echo "Token ${sonartoken}∫"
-                        }
-                    }
-                }
-            }*/
-
             stage('SonarQube Analysis') {
                 steps {
                     script {
@@ -71,7 +61,7 @@ def call() {
                         try{
 //                        withSonarQubeEnv('sonarqube') {
                             sh """
-                              ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=famiefi-api-utils -Dsonar.host.url=http://44.247.49.190:9002 -Dsonar.token=sqp_f425e7a673e249da66d856799b576a7dca6afccb -Dsonar.sources=app/ -Dsonar.working.directory=.scannerwork -X
+                              ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=famiefi-api-utils -Dsonar.token=sqp_f425e7a673e249da66d856799b576a7dca6afccb -Dsonar.sources=app/ -Dsonar.working.directory=.scannerwork -X
                             """
 
                         /*    sh """
