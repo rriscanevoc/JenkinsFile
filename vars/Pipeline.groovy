34683d5afbe5f6e97742cdfb65edb506a231a777
Ricardo Riscanevo Cotrina <rriscanevoc@ccxc.co>
2025-03-20 12:55:39 -0500

    Apply deployment

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -2,9 +2,10 @@ def call() {
     pipeline {
         agent any
         environment {
-        EC2_CREDENTIALS_ID = 'ec2-ssh-credential-utils'
-        PUBLIC_IP = ''
-
+            EC2_CREDENTIALS_ID = 'ec2-ssh-credential-utils'
+            Id_instance = 'calidad-v1-diggi-utils'
+            FORGE_COMPOSER = 'php8.1 /usr/local/bin/composer'
+            
         }
         
         stages {
@@ -51,7 +52,7 @@ def call() {
             stage('Conexión') {
                 steps {
                     script {
-                        withCredentials([string(credentialsId: 'calidad-v1-diggi-utils', variable: 'INSTANCE_ID')]) {
+                        withCredentials([string(credentialsId: 'Id_instance', variable: 'INSTANCE_ID')]) {
                             try{
                                 def publicIp = sh(
                                     script: '''
@@ -65,35 +66,35 @@ def call() {
                             }catch (Exception e) {
                             echo "Se encontraron problemas en ubicar la Ip del servidor."
                             error("Pipeline detenido por error en conexión.")
+                            }
                             
                             try{    
                                 sshagent([env.EC2_CREDENTIALS_ID]) {
                                     sh """
                                     ssh forge@${publicIp} \
                                     "set -e; \
-                                    echo "Obteniendo la IP privada..."; \
-                                    curl -s http://169.254.169.254/latest/meta-data/local-ipv4; \
-                                    echo "Listando archivos en /home/forge..."; \
-                                    cd /home/forge && ls -la"
+                                    echo "Desplegando..."; \
+                                    cd /home/forge/calidad-v1-diggi-utils && git pull origin calidad-viejo; \
+                                    php8.1 /usr/local/bin/composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader; \
+                                    ( flock -w 10 9 || exit 1; \
+                                        echo 'Restarting FPM...'; \
+                                        sudo -S service php8.1-fpm reload ) 9>/tmp/fpmlock; \
+
+                                        if [ -f artisan ]; then php8.1 artisan migrate --force;fi; \
+                                    echo '✅ Despliegue completado exitosamente.'"           
                                     """
                                 }
                             
                             }catch (Exception e) {
                             echo "Se encontraron problemas en el despliegue."
                             error("Pipeline detenido por error en error en despliegue.")
-                        }          
+                            }          
                         }
                     }
                 }
             }
         }
         post {
-            success {
-                echo " Despliegue completado exitosamente"
-            }
-            failure {
-                echo " El despliegue falló"
-            }   
             always {
                 cleanWs()
             }
--- vars/SonarQube.groovy
+++ vars/SonarQube.groovy
@@ -72,10 +72,8 @@ def call() {
                         } catch (Exception e) {
                             echo "Se encontró error. Revisa antes de continuar."
                             error("Pipeline detenido por exposición de credenciales.")
-                    }
-                   /* sh """
-                    /var/lib/jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonarscanner/bin/sonar-scanner -Dsonar.projectKey=famiefi-api-utils -Dsonar.host.url=http://44.247.49.190:9002 -Dsonar.token=sqp_f425e7a673e249da66d856799b576a7dca6afccb -Dsonar.sources=app/ -Dsonar.working.directory=.scannerwork -X
-                    """*/
+                        }
+
                     }
                 }
             }
